dc10a1bf491c78a2ed4960f583902c07857a32ff
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-26 17:50:40 -0300

    fix proxy problem to use with vite project

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8080",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+    port: 3000,
+  },
   plugins: [react()],
 });
