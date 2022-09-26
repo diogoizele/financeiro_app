8c349996d5c49a8c987f11fd3ae1b6fbfe9d8462
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-26 01:04:12 -0300

    creates auth hook

---
+++ src/hooks/auth/useAuth.hook.ts
@@ -0,0 +1,42 @@
+import { useCallback, useMemo } from "react";
+
+import { useUser } from "../../context";
+import { useHttp } from "../api/_base/useHttp.hook";
+
+type LoginParams = {
+  username: string;
+  password: string;
+};
+
+export function useAuth() {
+  const instance = useHttp({ baseURL: import.meta.env.VITE_API_URL });
+  const { user, setUser } = useUser();
+
+  const login = useCallback(async (payload: LoginParams) => {
+    try {
+      const response = await instance.post("/auth/login", null, {
+        auth: payload,
+      });
+
+      setUser(response?.data);
+    } catch (error) {
+      throw error;
+    }
+  }, []);
+
+  const logout = useCallback(async () => {
+    await instance.post("/logout");
+
+    localStorage.removeItem(import.meta.env.VITE_LOCAL_STORAGE_USER_KEY);
+    setUser(null);
+  }, []);
+
+  return useMemo(
+    () => ({
+      isAuth: !!user,
+      login,
+      logout,
+    }),
+    [user]
+  );
+}
