eea9cff94671f4f8e8cb9a80427b98681590cb2d
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-26 22:46:33 -0300

    adds new components to components index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export { Layout } from "./layout/layout.component";
+export { ExpenseCard } from "./expenseCard/expenseCard.component";
+export { ActivityActions } from "./activityActions/activityActions.component";
