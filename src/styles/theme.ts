ec95992888684eb1e70f76680e7c8f372a9eb9fd
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-26 22:49:15 -0300

    adds some gray colors in theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -9,5 +9,16 @@ export const THEME = {
     LIGHT: "#f8f9fa",
     DARK: "#343a40",
     WHITE: "#fff",
+    BLACK: "#000",
+    
+    GRAY_100: "#f8f9fa",
+    GRAY_200: "#e9ecef",
+    GRAY_300: "#dee2e6",
+    GRAY_400: "#ced4da",
+    GRAY_500: "#adb5bd",
+    GRAY_600: "#6c757d",
+    GRAY_700: "#495057",
+    GRAY_800: "#343a40",
+    GRAY_900: "#212529",
   },
 };
