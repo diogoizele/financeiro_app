6982693c4bd9978e7b16ee38158ce3ec500cd143
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-26 22:51:24 -0300

    render expenses and handle errors and loading in expense screen

--- src/screens/expense/expense.screen.tsx
+++ src/screens/expense/expense.screen.tsx
@@ -1,18 +1,83 @@
+import { useEffect, useState } from "react";
+
+import {
+  ActivityBasicResponse,
+  ActivityResponse,
+} from "../../@types/activity.type";
+import { Category } from "../../@types/category.type";
+
 import { ActivityHeader, NewExpenseModal } from "../../containers";
-import { useVisibility } from "../../hooks";
+import { useActivity, useVisibility } from "../../hooks";
+import { ExpenseCard } from "../../components";
 
 import * as styled from "./expense.styles";
+import { Empty, Spin } from "antd";
 
 export const Expense = () => {
+  const [expenses, setExpenses] = useState<ActivityResponse[]>([]);
+  const [getExpensesIsLoading, setGetExpensesIsLoading] = useState(false);
+  const [getExpensesError, setGetExpensesError] = useState(false);
+
   const { isVisible, hide, show } = useVisibility();
+  const { getActivityPage } = useActivity();
+
+  async function fetchExpenses() {
+    setGetExpensesIsLoading(true);
+    try {
+      const response = await getActivityPage({ category: Category.EXPENSE });
+
+      if (response) {
+        const { content } = response;
+
+        setExpenses(content as ActivityResponse[]);
+      }
+    } catch (error) {
+      setGetExpensesError(true);
+    }
+
+    setGetExpensesIsLoading(false);
+  }
+
+  function handleIncrementNewExpense(expense: ActivityBasicResponse) {
+    setExpenses((prevState) => [expense as ActivityResponse, ...prevState]);
+  }
+
+  useEffect(() => {
+    fetchExpenses();
+  }, []);
 
   return (
     <styled.ExpenseContainer>
       <ActivityHeader
-        title="Despesa"
+        title="Despesas"
         primaryAction={{ onClick: show, label: "Nova despesa" }}
       />
-      <NewExpenseModal open={isVisible} onClose={hide} />
+      <NewExpenseModal
+        open={isVisible}
+        onClose={hide}
+        onIncrementNewExpense={handleIncrementNewExpense}
+      />
+      {!getExpensesError && !getExpensesIsLoading ? (
+        <styled.ExpenseListContainer>
+          {expenses.map((expense) => (
+            <ExpenseCard key={expense.id} expense={expense} />
+          ))}
+          {expenses.length === 0 && (
+            <styled.ExpenseNoListContainer>
+              <Empty description="Nenhuma despesa encontrada" />
+            </styled.ExpenseNoListContainer>
+          )}
+        </styled.ExpenseListContainer>
+      ) : (
+        <styled.ExpenseNoListContainer>
+          {getExpensesError && !getExpensesIsLoading && (
+            <Empty description="Erro ao carregar despesas" />
+          )}
+          {!getExpensesError && (
+            <Spin spinning={getExpensesIsLoading} size="large" />
+          )}
+        </styled.ExpenseNoListContainer>
+      )}
     </styled.ExpenseContainer>
   );
 };
--- src/screens/expense/expense.styles.ts
+++ src/screens/expense/expense.styles.ts
@@ -5,3 +5,20 @@ export const ExpenseContainer = styled("div")(() => ({
   flexDirection: "column",
   width: "100%",
 }));
+
+export const ExpenseListContainer = styled("div")(() => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: "1rem",
+  width: "100%",
+  height: "100%",
+  overflow: "auto",
+  marginTop: "1rem",
+}));
+
+export const ExpenseNoListContainer = styled("div")(() => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100%",
+}));
