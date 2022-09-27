fd46818c9e2d2c761df037910133d55bc5c0584c
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-26 22:44:49 -0300

    adds main css file and setup moment in main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import moment from "moment";
+import 'moment/locale/pt-br.js';  
+
+import { UserProvider } from "./context";
 
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
-import { UserProvider } from "./context";
+import "./styles/main.css";
+
+moment.locale("pt-br");
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
